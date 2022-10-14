5dd1db6cf04631f9cca0cd6aed4ed742ff4b1a1f
William <p23wclegg@fairfieldprep.org>
2022-10-14 12:22:31 -0400

    labs 2 and 3

---
+++ lab-4-2.py
@@ -0,0 +1,8 @@
+#Lab-4-3
+School="Fairfield Prep"
+first_half=(School[:10])
+print (first_half)
+second_half=(School[10:])
+print (second_half)
+
+print (first_half + second_half)
---
+++ lab-4-3.py
@@ -0,0 +1,11 @@
+
+
+
+text=input("First word: ")
+text_2 =input("Second word: ")
+if text == (text_2):
+    print("The words are equal")
+elif text < (text_2):
+    print("First word is first")
+else:
+    print("Second word is first")
